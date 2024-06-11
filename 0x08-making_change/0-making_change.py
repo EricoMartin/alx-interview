ea01da1c702a8730808a0c9d35f9e71e3372577e
EricoMartin <ericomartin.0785@gmail.com>
2024-06-11 06:06:18 -0700

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,7 +17,7 @@ def makeChange(coins, total):
     for i in range(1, total + 1):
         for j in range(0, len(coins)):
             if coins[j] <= i:
-                    nextCoin[i] = min(nextCoin, nextCoin[i - coins[j]] + 1)
+                    nextCoin[i] = min(nextCoin[i], nextCoin[i - coins[j]] + 1)
 
     if nextCoin[total] > total:
         return -1
