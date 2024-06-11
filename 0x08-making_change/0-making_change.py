8d269525bcdf549d71a5a7af85d6e3db7289e91f
EricoMartin <ericomartin.0785@gmail.com>
2024-06-11 04:14:10 -0700

    0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" code to determine the fewest number of
+    coins needed to meet a given amount.
+"""
+
+
+def makeChange(coins, total):
+    """ main code """
+    if (total >= 1):
+        return changeCoin(coins, total, [0] * total + 1)
+    else:
+        return 0
+
+
+def changeCoin(coins, total, nextCoin):
+    """
+        code to make change breaking the problem
+        into subproblems and caching them with DP
+    """
+    if (total < 0):
+        return -1
+
+    if (total == 0):
+        return 0
+
+    if (nextCoin[total] != 0):
+        return nextCoin[total]
+
+    minim = sys.maxsize
+
+    for coin in coins:
+        change = changeCoin(coins, total - coin, nextCoin)
+        if (change >= 0 and change < minim):
+            minim = change + 1
+
+    nextCoin[total] = -1 if (minim == sys.maxsize) else minim
+    return nextCoin[total]
