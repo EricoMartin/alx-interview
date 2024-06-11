9559779518fd7cd29e5e912e680718459af269ba
EricoMax <47243793+EricoMartin@users.noreply.github.com>
2024-06-11 13:16:23 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,7 +17,9 @@ def makeChange(coins, total):
     for i in range(1, total + 1):
         for j in range(0, len(coins)):
             if coins[j] <= i:
-                nextCoin[i] = min(nextCoin[i], nextCoin[i - coins[j]] + 1)
+                smallCoin = nextCoin[i - coins[j]]
+                if smallCoin != sys.maxsize and smallCoin + 1 < nextCoin[i]:
+                    nextCoin[i] = smallCoin + 1
 
     if nextCoin[total] > total:
         return -1
