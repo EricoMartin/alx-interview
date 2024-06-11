ab7be451378f860c46461120c08aef23b9bb0a40
EricoMartin <ericomartin.0785@gmail.com>
2024-06-11 05:00:04 -0700

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,14 +5,30 @@
 
 
 def makeChange(coins, total):
-    """ main code """
-    if (total < 1):
-        return 0
+    """
+        code to make change breaking the problem
+        into subproblems and caching them with DP
+    """
 
-    return changeCoin(coins, total, [0] * (total + 1))
+    nextCoin = [total + 1] * (total + 1)
+    nextCoin[0] = 0
+
+    for i in range(1, total + 1):
+        for j in range(0, len(coins)):
+            if coins[j] <= i:
+                nextCoin[i] = min(nextCoin[i], nextCoin[i - coins[j]] + 1)
+
+    if nextCoin[total] > total:
+        return -1
+    else:
+        return nextCoin[total]
+#    if (total < 1):
+#        return 0
+
+#    return changeCoin(coins, total, [0] * (total + 1))
 
 
-def changeCoin(coins, total, nextCoin):
+'''def changeCoin(coins, total, nextCoin):
     """
         code to make change breaking the problem
         into subproblems and caching them with DP
@@ -37,4 +53,4 @@ def changeCoin(coins, total, nextCoin):
         nextCoin[total] = -1
     else:
         nextCoin = minim
-    return nextCoin[total]
+    return nextCoin[total]'''
