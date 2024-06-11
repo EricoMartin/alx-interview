89162c5f901ef908d00e6647dc7c465755fef4b4
EricoMartin <ericomartin.0785@gmail.com>
2024-06-11 05:47:38 -0700

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,7 +11,7 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    nextCoin = [total + 1] * (total + 1)
+    nextCoin = [sys.maxsize for i in range(total + 1)]
     nextCoin[0] = 0
 
     for i in range(1, total + 1):
